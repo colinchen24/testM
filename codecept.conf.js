4140d007b55ba7c6e0c52033d00880d00aa1684e
Colin Chen <colinc@XMMACBOOK15.local>
2019-10-16 12:56:48 +0800

    xx

--- codecept.conf.js
+++ codecept.conf.js
@@ -63,7 +63,7 @@ exports.config = {
                 "chromeOptions": chromeOptions,
                 "proxy": {
                   "proxyType": "manual",
-                  "httpProxy": '104.225.156.192:8085'
+                  "httpProxy": '176.122.147.10:8085'
                   // "noProxy": "127.0.0.1,localhost"
                 }
               }
--- helper/sendEmail.js
+++ helper/sendEmail.js
@@ -8,7 +8,7 @@ class sendEmail extends Helper{
           headers: {
             'Content-Type': 'application/json'
           },
-          url:  'http://104.225.156.192:2222/test/sendEmail',
+          url:  'http://176.122.147.10:2222/test/sendEmail',
           body: JSON.stringify(postdata)
         }, function(error, response, body){
           console.log(body)
@@ -21,7 +21,7 @@ class sendEmail extends Helper{
           headers: {
             'Content-Type': 'application/json'
           },
-          url:  'http://104.225.156.192:2222/test/getmonitor',
+          url:  'http://176.122.147.10:2222/test/getmonitor',
           body: JSON.stringify(body)
         }, function(error, response, body){
           // console.log(response.body)
@@ -36,7 +36,7 @@ class sendEmail extends Helper{
           headers: {
             'Content-Type': 'application/json'
           },
-          url:  'http://104.225.156.192:2222/test/track',
+          url:  'http://176.122.147.10:2222/test/track',
           body: JSON.stringify(data)
         }, function(error, response, body){
           // console.log(response.body)
