2a525e77261f897648d29418b114af501e2aa991
Colin Chen <colinc@XMMACBOOK15.local>
2019-10-20 20:13:52 +0800

    x

--- test/eastbay.js
+++ test/eastbay.js
@@ -34,81 +34,9 @@ Scenario('monitor nike', async function(I) {
 
     var now = dateFormat(new Date(), "isoDateTime");
 
-    I.amOnPage('https://www.eastbay.com/category/shoes/nike.html');
+    I.amOnPage('https://www.eastbay.com/product/nike-pg-3-mens/2607005.html');
     
-
-    for (var k = 0; k < list.length; k++) {
-
-        try {
-            if (k === 0 || (list[k-1].url !== list[k].url)){
-                if(k !==0){
-                    I.track(samesizes);
-                    samesizes = [];
-                }
-                
-                var productHtml = await I.executeScript(function(url) {
-                    var xmlHttp = new XMLHttpRequest();
-                    // var result = '';
-                    
-                    xmlHttp.open("GET", 'https://www.eastbay.com/product/nike-pg-3-mens/2607005.html', true); // true for asynchronous 
-                    xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-                    xmlHttp.send();
-
-                    var result = xmlHttp.onreadystatechange = function() {
-                        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-                            result = xmlHttp.responseText.split("\"c-form-field\"")[1].split("ProductDetails-form__label")[0];
-                            // console.log(result);
-                            return result;
-                        }
-                    // console.log(xmlHttp.responseText)
-                    }
-                    
-                    setTimeout(function(){return result;},1000)
-
-                    // return result;
-                 
-                }, list[k].url);
-            }
-
-            console.log(productHtml);
-
-            if(productHtml.indexOf('Size ' + list[k].size + ', out of stock') === -1){
-                availiabled = true;
-            } else {
-                availiabled = false;
-            }
-
-            now = dateFormat(new Date(), "isoDateTime");
-
-            if (availiabled) {
-
-                samesizes.push({
-                    "url": list[k].url,
-                    "size": list[k].size,
-                    "status": 'enabled',
-                    "time": now
-                })
-
-            } else {
-                samesizes.push({
-                    "url": list[k].url,
-                    "size": list[k].size,
-                    "status": 'disabled',
-                    "time": now
-                })
-            }
-            //forever running
-            if(k === list.length-1){
-                var list = await I.MonitorList({"frequency": process.env.TIME});
-                list = JSON.parse(list);
-                k = 0;
-            }
-        } catch (err) {
-            console.log(err)
-        }
-
-
-    }
+    
 
 
 });
