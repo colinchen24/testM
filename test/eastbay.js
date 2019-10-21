c18034f3bbe352b469293e70bc5a976d372c4ac0
Colin Chen <colinc@XMMACBOOK15.local>
2019-10-22 07:33:44 +0800

    x

--- test/eastbay.js
+++ test/eastbay.js
@@ -6,36 +6,71 @@ Scenario('monitor nike', async function(I) {
 
     // console.log(process.env.TIME)
 
-    var sleeptime=60;
+    var sleeptime = 60;
 
-    switch (process.env.TIME){
+    switch (process.env.TIME) {
         case "sh":
-            sleeptime=3;
+            sleeptime = 3;
             break;
         case "eb":
-            sleeptime=3;
+            sleeptime = 3;
             break;
     }
-
     // console.log(sleeptime);
-
-    var list = await I.MonitorList({"frequency": process.env.TIME});
+    var list = await I.MonitorList({
+        "frequency": process.env.TIME
+    });
     list = JSON.parse(list);
 
-    list = [{"url" : "https://www.eastbay.com/product/nike-pg-3-mens/2607005.html", "size": "07.0"},
-            {"url" : "https://www.eastbay.com/product/nike-pg-3-mens/2607005.html", "size": "07.5"},
-            {"url" : "https://www.eastbay.com/product/nike-pg-3-mens/2607005.html", "size": "08.0"},
-            {"url" : "https://www.eastbay.com/product/nike-pg-3-mens/2607005.html", "size": "08.5"},
-            {"url" : "https://www.eastbay.com/product/nike-pg-3-mens/2607005.html", "size": "09.0"}]
-
+    var availiabled = false;
+    var postHTML = '';
+    var now = dateFormat(new Date(), "isoDateTime");
     var samesizes = [];
 
-    var availiabled = false;
+    for (var k = 0; k < list.length; k++) {
+        if (k === 0 || list[k].url !== list[k - 1].url) {
+            console.log(samesizes);
+            if (samesizes && JSON.stringify(samesizes).indexOf('enabled') !== -1) {
+                await I.track(samesizes);
+                var samesizes = [];
+            }
+            await I.clearCookie();
+            I.amOnPage(list[k].url);
+            I.wait(3);
+            postHTML = await I.grabHTMLFrom('.c-form-field');
+            postHTML = postHTML.toString();
+            // console.log(postHTML);
+        }
 
-    var now = dateFormat(new Date(), "isoDateTime");
-
-    I.amOnPage('https://www.eastbay.com/product/nike-pg-3-mens/2607005.html');  
-    let postHTML = await I.grabHTMLFrom('.c-form-field');
-    console.log(postHTML)
+        if (postHTML.toString().indexOf('Size ' + list[k].size +', out of stock') === -1) {
+            availiabled = true;
+        } else {
+            availiabled = false;
+        }
+        now = dateFormat(new Date(), "isoDateTime");
+        if (availiabled) {
+            samesizes.push({
+                "url": list[k].url,
+                "size": list[k].size,
+                "status": 'enabled',
+                "time": now
+            })
+        } else {
+            samesizes.push({
+                "url": list[k].url,
+                "size": list[k].size,
+                "status": 'disabled',
+                "time": now
+            })
+        }
+        //forever running
+        if (k === list.length - 1) {
+            var list = await I.MonitorList({
+                "frequency": process.env.TIME
+            });
+            list = JSON.parse(list);
+            k = 0;
+        }
+    }
 
 });
