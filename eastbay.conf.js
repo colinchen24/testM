7c2998c1bd26ffbbe576afba7a1c3a1e7f2b20a1
Colin Chen <colinc@XMMACBOOK15.local>
2019-10-22 16:21:19 +0800

    x

--- eastbay.conf.js
+++ eastbay.conf.js
@@ -3,7 +3,7 @@ var host = config.get('host');
 var port = config.get('port');
 var chromeOptions = {
     "useAutomationExtension": false,
-    "args": ["--window-size=1800,1600","--no-sandbox","--disable-dev-shm-usage", "--disable-gpu", "--blink-settings=imagesEnabled=false"] //--headless //--headless
+    "args": ["--window-size=1800,1600","--no-sandbox","--disable-dev-shm-usage", "--disable-gpu", "--blink-settings=imagesEnabled=false"] //"--headless",
     // "args": ["--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_1_1 like Mac OS X) AppleWebKit/602.2.14 (KHTML, like Gecko)","--window-size=375,812"]
 };
 
