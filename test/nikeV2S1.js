8574394eb7b5ffe3e2d7394be92a3729f72adfc2
Colin Chen <colinc@XMMACBOOK15.local>
2019-10-29 10:22:28 +0800

    nike version 2

--- test/nikeV2S1.js
+++ test/nikeV2S1.js
@@ -24,7 +24,7 @@ Scenario('monitor nike', async function(I) {
 
     var slist =[];
 
-    for(var i=0; i< lparseInt(list.length/4)*2; i++){
+    for(var i=0; i< parseInt(list.length/4)*2; i++){
         slist.push(list[i])
     }
 
@@ -131,7 +131,7 @@ Scenario('monitor nike', async function(I) {
                 k = 0;
                 slist =[];
 
-                for(var i=0; i< lparseInt(list.length/4)*2; i++){
+                for(var i=0; i< parseInt(list.length/4)*2; i++){
                     slist.push(list[i])
                 }
             
