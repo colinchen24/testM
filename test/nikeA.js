a6f411d47ffd10735c016507cd35d21a72de68ee
Colin Chen <colinc@XMMACBOOK15.local>
2019-11-01 13:41:03 +0800

    x

--- test/nikeA.js
+++ test/nikeA.js
@@ -10,7 +10,7 @@ Scenario('monitor nike', async function(I) {
 
     switch (process.env.TIME){
         case "sh":
-            sleeptime=1;
+            sleeptime=10;
             break;
         case "h":
             sleeptime=30;
