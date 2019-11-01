41fcd4cde6b1a4a3e7873d5d61c412898b9a1f72
Colin Chen <colinc@XMMACBOOK15.local>
2019-11-01 11:25:29 +0800

    x

--- test/nikeA.js
+++ test/nikeA.js
@@ -10,7 +10,7 @@ Scenario('monitor nike', async function(I) {
 
     switch (process.env.TIME){
         case "sh":
-            sleeptime=4;
+            sleeptime=2;
             break;
         case "h":
             sleeptime=30;
@@ -61,6 +61,7 @@ Scenario('monitor nike', async function(I) {
                 
                 
                 // I.wait(2)
+                I.wait(sleeptime);
                 await I.amOnPage(list[k].url);
                 // I.saveScreenshot('result.jpg');
                 I.wait(sleeptime);
