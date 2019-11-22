a64bc279d4879cd776a79d989448498fe4f0a966
colinc <colin.chen24@gmail.com>
2019-11-22 19:47:29 +0800

    x

--- test/nikeA.js
+++ test/nikeA.js
@@ -49,13 +49,13 @@ Scenario('monitor nike', async function(I) {
     var falseFlag = false;
     for (k; k < list.length; k++) {
 
-        
+
 
         try {
             if (k === 0 || (k !== 0 && list[k - 1].url !== list[k].url)) {
 
                 console.log('k = 0 or url different ==' + k + "url " + list[k].url + " size " + list[k].size);
-                
+
                 if (samesizes.length !== 0) {
                     console.log('save track: ' + list[k].url);
                     await I.track(samesizes);
@@ -70,7 +70,7 @@ Scenario('monitor nike', async function(I) {
                 // I.saveScreenshot('result.jpg');
                 // I.wait(sleeptime);
                 htmlcontext = "htmlcontext";
-                
+
                 htmlcontext = await I.executeScript(function(url, size, price) {
                     if (!"".replaceAll) {
                         String.prototype.replaceAll = function(search, replacement) {
@@ -91,31 +91,26 @@ Scenario('monitor nike', async function(I) {
                     } else if (price !== '' && price && document.querySelectorAll('[data-test="product-price"]') && document.querySelectorAll('[data-test="product-price"]')[0].innerText.split('￥')[1].replace(',', '') !== price) {
                         return 'price updated';
                     } else if (document.getElementById('buyTools')) {
-                        if(document.getElementById('buyTools').innerHTML.indexOf(list[k].size) !== -1){
-                            return document.getElementById('buyTools').innerHTML;
-                        } else {
-                            setTimeout(function() {
-                                return document.getElementById('buyTools').innerHTML;
-                            }, 2000);
-                        }
-                        
-                    } else if(document.getElementById('RightRail') && document.getElementById('RightRail').innerText.indexOf('售罄：') !== -1){
+
+                        return document.getElementById('buyTools').innerHTML;
+
+                    } else if (document.getElementById('RightRail') && document.getElementById('RightRail').innerText.indexOf('售罄：') !== -1) {
                         return "out of stock"
                     }
                 }, list[k].url, list[k].size, list[k].price);
 
                 if (htmlcontext !== "Forbidden" && htmlcontext !== "htmlcontext") {
-                    if(sleeptime > 0){
+                    if (sleeptime > 0) {
                         sleeptime--;
                     }
-                } else if(htmlcontext === "Forbidden"){
-                    if(sleeptime < 5) {
+                } else if (htmlcontext === "Forbidden") {
+                    if (sleeptime < 5) {
                         sleeptime++;
                     }
                 }
 
                 console.log("====================" + sleeptime + "====================");
-                if( sleeptime > 0){
+                if (sleeptime > 0) {
                     I.wait(sleeptime);
                 }
                 console.log(htmlcontext);
@@ -125,24 +120,61 @@ Scenario('monitor nike', async function(I) {
             availiabled = false;
             falseFlag = false;
 
-            
             if (htmlcontext !== "Forbidden" && htmlcontext !== "htmlcontext") {
                 // console.log('access');
 
                 if (htmlcontext === "url changed" || htmlcontext === "out of stock") {
                     // console.log('==== 1');
                     availiabled = false;
-                    falseFlag =true;
+                    falseFlag = true;
                 } else if (htmlcontext === 'new updated') {
                     // console.log('==== 2');
                     availiabled = true;
                 } else if (htmlcontext === 'price updated') {
                     // console.log('==== 3');
                     availiabled = true;
-                } else if (htmlcontext.split(list[k].size + '</label>').length > 1 && htmlcontext.split(list[k].size + '</label>')[0].split("<input")[htmlcontext.split(list[k].size + '</label>')[0].split("<input").length -1 ].indexOf('disabled') === -1) {
+                } else if (htmlcontext.indexOf(list[k].size) === -1) {
+                    I.await(2);
+
+                    htmlcontext = await I.executeScript(function(url, size, price) {
+                        if (!"".replaceAll) {
+                            String.prototype.replaceAll = function(search, replacement) {
+                                var target = this;
+                                return target.replace(new RegExp(search, 'g'), replacement);
+                            };
+                        }
+
+                        if (document.body.innerText.indexOf('NikePlus') === -1 || document.body.innerText.indexOf('Forbidden access') !== -1) {
+                            // console.log('no document');
+                            return "Forbidden"
+                        } else if (window.location.href !== url) {
+                            return "url changed";
+                        } else if (url === 'https://www.nike.com/cn/w/new-shoes-3n82yzy7ok?sort=newest' && document.getElementsByClassName('product-card__body').length > 2 &&
+                            (document.getElementsByClassName('product-card__body')[0].innerText.replaceAll('\n', '').indexOf(size.split('&')[0]) === -1 || document.getElementsByClassName('product-card__body')[1].innerText.replaceAll('\n', '').indexOf(size.split('&')[1]) === -1 || document.getElementsByClassName('product-card__body')[2].innerText.replaceAll('\n', '').indexOf(size.split('&')[2]) === -1)
+                        ) {
+                            return 'new updated'
+                        } else if (price !== '' && price && document.querySelectorAll('[data-test="product-price"]') && document.querySelectorAll('[data-test="product-price"]')[0].innerText.split('￥')[1].replace(',', '') !== price) {
+                            return 'price updated';
+                        } else if (document.getElementById('buyTools')) {
+
+                            return document.getElementById('buyTools').innerHTML;
+
+                        } else if (document.getElementById('RightRail') && document.getElementById('RightRail').innerText.indexOf('售罄：') !== -1) {
+                            return "out of stock"
+                        }
+                    }, list[k].url, list[k].size, list[k].price);
+
+                    if (htmlcontext.split(list[k].size + '</label>').length > 1 && htmlcontext.split(list[k].size + '</label>')[0].split("<input")[htmlcontext.split(list[k].size + '</label>')[0].split("<input").length - 1].indexOf('disabled') === -1) {
+                        // console.log('==== 4');
+                        availiabled = true;
+                    }
+
+                } else if (htmlcontext.split(list[k].size + '</label>').length > 1 && htmlcontext.split(list[k].size + '</label>')[0].split("<input")[htmlcontext.split(list[k].size + '</label>')[0].split("<input").length - 1].indexOf('disabled') === -1) {
                     // console.log('==== 4');
                     availiabled = true;
                 }
+
+
 
                 var now = getZoneTime();
                 if (availiabled) {
@@ -155,7 +187,7 @@ Scenario('monitor nike', async function(I) {
                         "utctime": now
                     })
 
-                } else if(falseFlag) {
+                } else if (falseFlag) {
                     samesizes.push({
                         "url": list[k].url,
                         "size": list[k].size,
@@ -164,7 +196,7 @@ Scenario('monitor nike', async function(I) {
                         "utctime": now
                     })
                 }
-            } 
+            }
 
             //forever running
             if (k === list.length - 1) {
